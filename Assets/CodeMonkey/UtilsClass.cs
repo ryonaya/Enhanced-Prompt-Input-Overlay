6a59d2e2cb661cc8dac9f9811f4f636574380c6a
daidaijr <daidai680608@gmail.com>
2022-12-08 22:10:05 +0800

    Feature: Basic context setup

---
+++ Assets/CodeMonkey/UtilsClass.cs
@@ -0,0 +1,39 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading the Code Monkey Utilities
+    I hope you find them useful in your projects
+    If you have any questions use the contact form
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace CodeMonkey {
+
+    /*
+     * Various assorted utilities functions
+     * */
+    public static class UtilsClass
+    {
+        // Get Mouse Position in World with Z = 0f
+        public static Vector3 GetMouseWorldPosition(Camera cam) {
+            Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, cam);
+            vec.z = 0f;
+            return vec;
+        }
+
+        private static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera) {
+            Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+            return worldPosition;
+        }
+    }
+
+}
---
+++ Assets/Scripts/TransparentWindow.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Runtime.InteropServices;
+using CodeMonkey;
+using UnityEngine;
+
+public class TransparentWindow : MonoBehaviour
+{
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetActiveWindow();
+
+    [DllImport("user32.dll")]
+    private static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
+
+    [DllImport("user32.dll")]
+    private static extern int SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
+
+    [DllImport("user32.dll")]
+    static extern int SetLayeredWindowAttributes(IntPtr hWnd, uint crKey, byte bAlpha, uint dwFlags);
+
+    private IntPtr hWnd;
+    private const int GWL_EXSTYLE = -20;
+    private const uint WS_EX_LAYERED = 0x00080000;
+    private const uint WS_EX_TRANSPARENT = 0x00000020;
+    private const uint LWA_COLORKEY = 0x00000001;
+    private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+
+    private Camera main;
+    
+    private struct Margins
+    {
+        public int cxLeftWidth;
+        public int cxRightWidth;
+        public int cyTopHeight;
+        public int cyBottomHeight;
+    }
+
+    [DllImport("Dwmapi.dll")]
+    private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref Margins margins);
+
+    private void Awake()
+    {
+        hWnd = GetActiveWindow();
+    }
+
+    private void Start()
+    {
+        main = Camera.main;    
+    #if !UNITY_EDITOR
+        Margins margins = new Margins { cxLeftWidth = -1 };
+        DwmExtendFrameIntoClientArea(hWnd, ref margins);
+        SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT);
+        SetWindowPos(hWnd, HWND_TOPMOST, 0,0,0,0,0);
+    #endif
+        Application.runInBackground = true;
+    }
+
+    private void Update()
+    {
+        Ray ray = main.ScreenPointToRay(Input.mousePosition);
+        SetClickThrough(!Physics2D.Raycast(ray.origin, ray.direction));
+    }
+    
+    private void SetClickThrough(bool clickThrough)
+    {
+        if (clickThrough)
+        {
+            SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT);
+        }
+        else
+        {
+            SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED);
+        }
+    }
+}
